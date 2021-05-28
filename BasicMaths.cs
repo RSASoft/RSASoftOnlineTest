c4df1ff9f63125f35f4cdd31e4ea6378871a2796
AJ <ajdewilzin@rsasoft.com>
2021-05-28 11:17:08 +0200

    Initial commit

---
+++ BasicMaths.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSASoftOnline;
+using System;
+
+namespace RSASoftOnlineTest
+{
+    [TestClass]  
+    public class BasicMaths
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            RSASoftOnline.BasicMaths bm = new RSASoftOnline.BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            RSASoftOnline.BasicMaths bm = new RSASoftOnline.BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            RSASoftOnline.BasicMaths bm = new RSASoftOnline.BasicMaths();
+            double res = bm.Divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            RSASoftOnline.BasicMaths bm = new RSASoftOnline.BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
