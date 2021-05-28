fcee3691dfd8ca75f39b03618a19b84e4c232c56
AJ <ajdewilzin@rsasoft.com>
2021-05-28 11:19:26 +0200

    Initial commit
    Initial commit

---
+++ RSASoftOnline/OrderHeaderDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSASoftOnline
+{
+    public class OrderHeaderDetails
+    {
+        public string PONumber { get; set; }
+        public string CustomerOrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public float Total { get; set; }
+        public int NumberOfLines { get; set; }
+        public List<OrderLineDetails> LineDetails { get; set; }
+    }
+}
---
+++ RSASoftOnline/OrderLineDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSASoftOnline
+{
+    public class OrderLineDetails
+    {
+        public string StockCode { get; set; }
+        public string StockDescription { get; set; }
+        public int Quantity { get; set; }
+        public float CustomerPrice { get; set; }
+        public float Total { get; set; }
+    }
+}
